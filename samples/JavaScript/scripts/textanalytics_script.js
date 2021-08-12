ae28b12e30035b71d042a51a0d64442520441eac
Ayako Omori <ayomori@microsoft.com>
2021-08-13 01:40:29 +0900

    Summarize sample (JS) commit
    Text Analytics Summarize sample (HTML/JavaScript) commit

---
+++ samples/JavaScript/scripts/textanalytics_script.js
@@ -0,0 +1,116 @@
+$(function () {
+
+    var outputDiv = $("#OutputDiv");
+    var resultUrlDiv = $("#ResultUrlDiv");
+
+    // Text Analytics の Subscription Key と URL をセット
+    // Azure Portal 画面に表示される URL および Key をコピーしてください
+    var subscriptionKey = "YOUR_API_KEY";
+    var endpoint = "https://YOUR_SERVICE_URL.cognitiveservices.azure.com/";
+
+    // Text Analytics 呼び出し URL をセット
+    var webSvcUrl = endpoint + "text/analytics/v3.2-preview.1/analyze";
+
+    //文章の Post    
+    var postData = function () {
+
+        var textData = document.getElementById('TextData').value;
+        var analysisData = {
+            'displayName': 'tasks_20210811',
+            'analysisInput': {
+                'documents': [
+                    {
+                        'id': '001',
+                        'text': textData,
+                        'language': 'ja'
+                        }
+                ]
+            },
+            'tasks': {
+                'extractiveSummarizationTasks': [
+                    {
+                        'parameters': {
+                            'model-version': 'latest',
+                            'loggingOptOut': false,
+                            'stringIndexType': 'TextElement_v8',
+                            'sentenceCount': 1,
+                            'sortBy': 'Offset'
+                        }
+                    }
+                ]
+            }
+        };
+    
+        // Text Analytics にデータを Post
+        fetch( webSvcUrl, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Ocp-Apim-Subscription-Key': subscriptionKey,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(analysisData)
+
+        }).then((res)=>{
+
+            outputDiv.text("On process: posting data...");
+
+            if (!res.ok) {
+                outputDiv.text("ERROR!: failed to send text data.");
+            }
+            return(res.headers.get('operation-location'));
+
+        }).then((resultUrl)=>{
+                // 結果取得URL を取得
+                resultUrlDiv.text(resultUrl);
+                outputDiv.text("データを送信しました。結果を取得するには [Result] をクリックしてください");
+        });
+    };
+
+    // 結果の取得
+    var getResult = function () {
+
+        outputDiv.text("On process: checking analysis task status...");
+
+        var resultUrl = document.getElementById('ResultUrlDiv').textContent;
+        fetch( resultUrl, {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Ocp-Apim-Subscription-Key': subscriptionKey
+                }
+        }).then((res)=>{
+
+            if (!res.ok) {
+                outputDiv.text("ERROR!: failed to access to result.");
+            }
+            return(res.json() );
+
+        }).then((json)=>{
+
+            // 分析結果が取得できた場合
+            if (json.status == "succeeded")
+            {
+                var outputText = "<h3>" + "結果:" + "</h3>";
+                outputText += json.tasks.extractiveSummarizationTasks[0].results.documents[0].sentences[0].text;
+                outputDiv.html(outputText);                
+            }
+            else
+            {
+                outputDiv.text("分析中です。しばらくしてから再度 [結果の取得] をクリックしてください");
+            }
+        });
+
+    };
+
+    // データの送信
+    $("#PostData").on('click', function(e){
+        postData();
+    });
+
+    // 結果取得＆表示
+    $("#GetResult").on('click', function(e){
+        getResult();
+    });
+
+});
